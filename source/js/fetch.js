d3042f996fb951cb5e5e696cbdfccbfef5ca8d2a
andreykyr <GubernatorK@yandex.ru>
2023-07-12 19:33:50 +0300

    исправления по итогам раздела

--- source/js/fetch.js
+++ source/js/fetch.js
@@ -5,8 +5,6 @@ import { filterAds } from './filter-ads.js';
 // Load data
 const AD_COUNT = 10;
 
-let newArray = [];
-
 fetch('https://23.javascript.pages.academy/keksobooking/data')
   .then((response) => response.json())
   .then((ads) => {
@@ -43,4 +41,4 @@ const createFetch = (body, onSuccess, onError) => () => {
 }
 
 
-export {createFetch, newArray};
+export {createFetch};
